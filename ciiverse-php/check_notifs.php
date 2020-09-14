df6f4a8bb8e6d449427849b11f61fc93c7f10e64
SRGNation <32860440+SRGNation@users.noreply.github.com>
2020-09-14 11:00:07 -0700

    bruh...

--- ciiverse-php/check_notifs.php
+++ ciiverse-php/check_notifs.php
@@ -7,3 +7,7 @@ if (!empty($_SESSION['loggedin'])) {
 	$get_notifs = $db->query("SELECT * FROM notifs WHERE notif_to = '".$_SESSION['ciiverseid']."' AND rd_notif = 0");
 	$notif_count = mysqli_num_rows($get_notifs);
 
+	echo json_encode(array('success' => 1, 'notifs' => array('unread_count' => $notif_count)), JSON_FORCE_OBJECT);
+}
+
+?>
